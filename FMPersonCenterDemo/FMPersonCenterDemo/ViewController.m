11f084d603073daa8eae3420c78df401860b773d
周发明 <847881570@qq.com>
2017-04-18 16:57:24 +0800

    开发分支提交测试

--- FMPersonCenterDemo/FMPersonCenterDemo/ViewController.m
+++ FMPersonCenterDemo/FMPersonCenterDemo/ViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"新建开发分支测试");
 }
 
 - (void)didReceiveMemoryWarning {
